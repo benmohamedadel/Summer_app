03996cc73ddff882184bf88564727c9f9540f161
Adel <benmohamedadel85@gmail.com>
2022-08-10 00:30:50 +0100

    acceuil a 3 pages

--- summer-app/src/app/app.module.ts
+++ summer-app/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -18,12 +22,13 @@ import { RegisterComponent } from './register/register.component';
       LoginComponent,
       CoursesComponent,
       RegisterComponent,
+      DashboardComponent,
 
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([{path :'home',component:AcceuilComponent},{path:'' ,redirectTo:'home',pathMatch:'full'},{path:'register',component:RegisterComponent},
-  {path:'courses',component:CoursesComponent},{path:'login',component:LoginComponent}])
+  {path:'courses',component:CoursesComponent},{path:'login',component:LoginComponent},{path:'Dashboard',component:DashboardComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
